29d40c214642614b286d6b04b920e4c2a97e7e74
Allan021 <allancastro1912@gmail.com>
2021-09-06 11:59:23 -0600

    agregando el header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 import BtnHamburguer from "./BtnHamburguer";
 import Menu from "./Menu";
 
 export const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log("Cambio");
+    setOpenMenu(false);
+  }, [location]);
 
   return (
     <header className="header">
